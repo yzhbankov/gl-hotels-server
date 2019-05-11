acc497f7dc58d0d40f07bc80d2805a1bdf794cc3
yzhbankov <yzhbankov@gmail.com>
2019-05-11 22:49:47 +0300

    rebuild

--- build/server/auth/router.js
+++ build/server/auth/router.js
@@ -8,19 +8,29 @@ const router = express.Router();
 const config = require('./../../config');
 const handlers = require('./handlers');
 const Users = require('../users');
-router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
-    const token = jwt.sign(req.user, config.jwtsecret, { expiresIn: '24h' });
-    return res.json({ user: req.user, token });
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res, next) => {
+    try {
+        const token = jwt.sign(req.user, config.jwtsecret, { expiresIn: '24h' });
+        return res.json({ user: req.user, token });
+    }
+    catch (err) {
+        next(err);
+    }
 });
 router.get('/logout', passport.authenticate('jwt', { session: false }), (req, res) => {
     req.logout();
     res.send('You are logout');
 });
-router.get('/check_token', passport.authenticate('jwt', { session: false }), (req, res) => tslib_1.__awaiter(this, void 0, void 0, function* () {
-    const token = req.headers.authorization;
-    const decoded = jwt.verify(token.replace('Bearer ', ''), config.jwtsecret);
-    const user = yield Users.getUsers(decoded.login, {});
-    res.send(Object.assign({}, user));
+router.get('/check_token', passport.authenticate('jwt', { session: false }), (req, res, next) => tslib_1.__awaiter(this, void 0, void 0, function* () {
+    try {
+        const token = req.headers.authorization;
+        const decoded = jwt.verify(token.replace('Bearer ', ''), config.jwtsecret);
+        const user = yield Users.getUsers(decoded.login, {});
+        res.send(Object.assign({}, user));
+    }
+    catch (err) {
+        next(err);
+    }
 }));
 router.post('/sign_up', handlers.signUpHandler);
 module.exports = router;
